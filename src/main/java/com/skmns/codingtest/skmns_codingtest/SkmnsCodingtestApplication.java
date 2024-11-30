d4722e2a4c3423b0df8f73d5ea48d5f37155e92b
peteryu24 <67302252+peteryu24@users.noreply.github.com>
2024-11-30 22:46:47 +0900

    초기 환경 세팅 완료 #1

---
+++ src/main/java/com/skmns/codingtest/skmns_codingtest/SkmnsCodingtestApplication.java
@@ -0,0 +1,13 @@
+package com.skmns.codingtest.skmns_codingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkmnsCodingtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkmnsCodingtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skmns/codingtest/skmns_codingtest/SkmnsCodingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skmns.codingtest.skmns_codingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkmnsCodingtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
