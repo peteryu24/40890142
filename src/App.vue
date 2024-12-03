56e23ae4fad159f070563f1a6a6318c182eb5a8f
peteryu24 <67302252+peteryu24@users.noreply.github.com>
2024-12-04 05:18:04 +0900

    view

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <EntryPage msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EntryPage from "./components/EntryPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    EntryPage,
+  },
+};
 </script>
 
 <style>
