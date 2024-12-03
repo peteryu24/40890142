004e9323f2983f1fdcab5ea40c286b3488019cd1
peteryu24 <67302252+peteryu24@users.noreply.github.com>
2024-12-04 04:07:30 +0900

    불필요한 import문 제거 #14

--- src/main/java/com/skmns/codingtest/controller/ArticleController.java
+++ src/main/java/com/skmns/codingtest/controller/ArticleController.java
@@ -11,7 +11,6 @@ import com.skmns.codingtest.vo.ArticleVO;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
--- src/main/java/com/skmns/codingtest/vo/ArticleVO.java
+++ src/main/java/com/skmns/codingtest/vo/ArticleVO.java
@@ -1,7 +1,6 @@
 package com.skmns.codingtest.vo;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class ArticleVO {
     private Long articleId;
